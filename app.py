a5514125d4afcdf695c38e39440d1802291c4f95
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2025-04-21 19:49:14 -0400

    Pequeños avances en la barra lateral de la interfaz

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class App(ctk.CTk):
         self.logo_imagen = ctk.CTkImage(Image.open(f"{self.img_folder}/logo.png"), size=(240, 80))
 
         # Columnas de la ventana principal
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=0)
         self.grid_columnconfigure(1, weight=3)
         self.grid_rowconfigure(0, weight=1)
 
@@ -23,12 +23,39 @@ class App(ctk.CTk):
         self.nav_frame = ctk.CTkFrame(self)
         self.nav_frame.grid(row=0, column=0, sticky="nswe")
 
+        # Frame para el perfil o el inicio de sesión
+        self.profile_frame = ctk.CTkFrame(self.nav_frame)
+        self.profile_frame.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+        self.profile_frame.grid_rowconfigure(0, weight=1)
+
+        # Al iniciar la app se estará no registrado, por lo que se mostrará el botón de registro o inicio de sesión
+        self.iniciar_label = ctk.CTkLabel(self.profile_frame, text="Tiene una cuenta?")
+        self.iniciar_label.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        self.iniciar_button = ctk.CTkButton(self.profile_frame, text="Iniciar sesión", border_spacing=10, text_color="white", hover_color="seagreen")
+        self.iniciar_button.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+
+        # Botón para registrarse
+        self.registro_label = ctk.CTkLabel(self.profile_frame, text="No tiene una cuenta?")
+        self.registro_label.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+        self.registro_button = ctk.CTkButton(self.profile_frame, text="Registrarse", border_spacing=10, text_color="white", hover_color="seagreen")
+        self.registro_button.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
+
+        # Boton para ir al mapa
+        self.map_button = ctk.CTkButton(self.nav_frame, text="Mapa", corner_radius=0, border_spacing=10, text_color="white", fg_color="darkseagreen", hover_color="darkseagreen4", command=lambda: self.color_selected_nav_button(self.map_button))
+        self.map_button.grid(row=2, column=0, sticky="ew")
+
         # Logo
         self.logo_label = ctk.CTkLabel(self.nav_frame, text="", image=self.logo_imagen)
         self.logo_label.grid(row=0, column=0, padx=10, pady=10)
 
-    def on_button_click(self):
-        print("Button clicked!")
+    def color_selected_nav_button(self, boton):
+        # Cambia el color del botón seleccionado y restablece los demás
+        for button in self.nav_frame.winfo_children():
+            if isinstance(button, ctk.CTkButton):
+                button.configure(fg_color="gray74", hover_color="gray44")
+        boton.configure(fg_color="darkseagreen", hover_color="darkseagreen4")
+
+    
 
 app = App()
 app.mainloop()
